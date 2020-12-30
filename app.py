e23115b06e83576c74b777ce1e73549b3c99a4dd
xban360 <xban360@gmail.com>
2020-12-30 08:54:41 +0000

    startup this project

---
+++ app.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import configparser
+
+import random
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+@app.route("/")
+def home():
+    return "Welcome Hook"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    try:
+        print(body, signature)
+        handler.handle(body, signature)
+        
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Louis: ' + event.message.text)
+    )
+
+if __name__ == "__main__":
+    app.run()
